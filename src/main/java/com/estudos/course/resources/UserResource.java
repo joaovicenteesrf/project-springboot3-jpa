8a8762dcafb17dde900b47fb0ad10609746e03ab
João Vicente <joaovicenterangel@gmail.com>
2023-02-26 19:38:34 -0300

    feature/SPRING - Adding User and User resource

---
+++ src/main/java/com/estudos/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.estudos.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudos.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "João", "joao@gmail.com", "21992950519", "12345678");
+		return ResponseEntity.ok().body(u);
+	}
+}
